27c53b2b7837345f82ff9a74790d2a4c279ba12e
nrmadi02 <ahmadinur2202@gmail.com>
2025-12-09 22:16:29 +0800

    refactor: consolidate email client functions

--- src/tasks/email/clients/send-email-async.ts
+++ src/tasks/email/clients/send-email-async.ts
@@ -5,3 +5,8 @@ export const sendVerificationEmailAsync = async (emails: string[], token: string
   const job = await emailQueue.add(EMAIL_TASK.SendVerificationEmail, { emails, token });
   console.info(`Job ${job.id} added to queue. Task scheduled for ${EMAIL_TASK.SendVerificationEmail}, emails: ${emails}`);
 };
+
+export const sendResetPasswordEmailAsync = async (emails: string[], token: string) => {
+  const job = await emailQueue.add(EMAIL_TASK.SendResetPasswordEmail, { emails, token });
+  console.info(`Job ${job.id} added to queue. Task scheduled for ${EMAIL_TASK.SendResetPasswordEmail}, emails: ${emails}`);
+};
--- src/tasks/email/processor.ts
+++ src/tasks/email/processor.ts
@@ -1,8 +1,10 @@
 import type { Job } from 'bullmq';
 import { sendEmailVerificationJob } from './jobs/send-email-verification.job';
+import { sendResetPasswordEmailJob } from './jobs/send-reset-password-email.job';
 
 export const EMAIL_TASK = {
   SendVerificationEmail: 'send_verification_email',
+  SendResetPasswordEmail: 'send_reset_password_email',
 } as const;
 
 export type EmailTaskName = typeof EMAIL_TASK[keyof typeof EMAIL_TASK];
@@ -11,6 +13,8 @@ export const emailWorkerProcessor = async (job: Job) => {
   switch (job.name as EmailTaskName) {
     case EMAIL_TASK.SendVerificationEmail:
       return await sendEmailVerificationJob(job.data.emails, job.data.token);
+    case EMAIL_TASK.SendResetPasswordEmail:
+      return await sendResetPasswordEmailJob(job.data.emails, job.data.token);
     default:
       console.warn('[email] Unknown task', job.name, job.data);
   }
