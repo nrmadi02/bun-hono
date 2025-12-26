6bdbe779b20239bb8552a5ed28e6584bde9cdbaa
nrmadi02 <ahmadinur2202@gmail.com>
2025-12-26 22:04:36 +0800

    feat: add email verification model and migration

---
+++ prisma/migrations/20251226131111_add_email_verification_model/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "EmailVerification" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "EmailVerification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "EmailVerification_token_key" ON "EmailVerification"("token");
+
+-- CreateIndex
+CREATE INDEX "EmailVerification_userId_idx" ON "EmailVerification"("userId");
+
+-- AddForeignKey
+ALTER TABLE "EmailVerification" ADD CONSTRAINT "EmailVerification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
